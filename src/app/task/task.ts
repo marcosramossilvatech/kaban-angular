682cf8b0bb791a355eefac2b816605da707ef7d1
Marcos <mrsilvatdas@gmail.com>
2023-09-27 23:49:18 -0300

    Primeiro commit

---
+++ src/app/task/task.ts
@@ -0,0 +1,11 @@
+export interface Task {
+  id?: string;
+  title: string;
+  description: string;
+}
+
+export interface Quadro {
+  id?: string;
+  title: string;
+  description: string;
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'kanban-85647',
+    appId: '1:522424839130:web:d7dd395207b7601e67eee3',
+    storageBucket: 'kanban-85647.appspot.com',
+    apiKey: 'AIzaSyBPWiF6YdcWsM77_HMLjE35ZOk4WsMqiXM',
+    authDomain: 'kanban-85647.firebaseapp.com',
+    messagingSenderId: '522424839130',
+    measurementId: 'G-0CWJQGYGYL',
+  },
+};
+
